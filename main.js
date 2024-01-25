ae22116a507fcacda986cddbaf2d1ef461de3fd6
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-01-25 19:55:14 +0100

    exercise completed bonus

--- main.js
+++ main.js
@@ -4,9 +4,11 @@
   createApp({
     data() {
       return {
-        Hello: 'Hello Vue!',
+        Hello: 'Hello World!',
+        Mitico: 'Miticooooo!',
         colore: 'text-light',
-        back: 'bg-danger'
+        back: 'back',
+        url: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Homer-Simpson.The-Simpsons.webp'
         
       }
     }
